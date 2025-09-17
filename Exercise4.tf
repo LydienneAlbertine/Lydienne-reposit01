6ac1bbdec70ddd1b8d8816f0c769b02b58ccb512
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-17 19:29:18 -0400

    Update Exercise4.tf

--- Exercise4.tf
+++ Exercise4.tf
@@ -1,9 +1,9 @@
-variable "items" {
+variable "items_ex4" {
  type    = list(string)
  default = ["b", "a", "c", "a", "b"]
 }
 locals {
- unique_sorted = sort(distinct(var.items))
+ unique_sorted = sort(distinct(var.items_ex4))
  csv           = join(",", local.unique_sorted)
 }
 output "unique_sorted" { value = local.unique_sorted }
