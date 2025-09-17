47d997c41b62fc4e7df3de433a7e62382099cf9c
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-17 19:27:38 -0400

    Update Exercise4.tf

--- Exercise4.tf
+++ Exercise4.tf
@@ -1,1 +1,11 @@
+variable "items" {
+ type    = list(string)
+ default = ["b", "a", "c", "a", "b"]
+}
+locals {
+ unique_sorted = sort(distinct(var.items))
+ csv           = join(",", local.unique_sorted)
+}
+output "unique_sorted" { value = local.unique_sorted }
+output "csv"           { value = local.csv }
 
