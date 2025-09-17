6a298e82e5b45c1a1f479c7f4ec7d352d34d7878
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-17 19:05:50 -0400

    Update Exercise 3.tf

--- Exercise 3.tf
+++ Exercise 3.tf
@@ -4,15 +4,15 @@ variable "raw_scores" {
 }
 locals {
  score_pairs = [
-   for s in var.raw_scores : {
-     name  = split(":", s)[0]
-     score = tonumber(split(":", s)[1])
-   }
+ for s in var.raw_scores : {
+ name  = split(":", s)[0]
+ score = tonumber(split(":", s)[1])
+ }
  ]
  scores_map   = { for p in local.score_pairs : p.name => p.score }
  average      = length(local.score_pairs) = 0 ? 0 :
- sum([for p in local.score_pairs : p.score]) / length(local.score_pairs)
+ sum=([for p in local.score_pairs : p.score]) / length(local.score_pairs)
 }
 output "scores_map" { value = local.scores_map }
 output "avg_score"  { value = local.average    }
-
+}
