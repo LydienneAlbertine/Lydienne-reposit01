ee260af19d27f98ec4c842e258043c213cbe5f53
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-17 20:15:18 -0400

    Update Exercise8.tf

--- Exercise8.tf
+++ Exercise8.tf
@@ -1,1 +1,16 @@
-
+# Append Hello before each name
+variable "names" {
+  type    = list(string)
+  default = ["Alice", "Bob", "Carol"]
+}
+locals {
+  greetings = []
+}
+locals {
+  greetings = flatten([
+    for n in var.names : [ "Hello ${n}" ]
+  ])
+}
+output "greetings" {
+  value = local.greetings
+}
