704ba4392cd0b81aa2155adb3527d02b04a5c5a3
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-19 18:41:10 -0400

    Update Exercise10.tf

--- Exercise10.tf
+++ Exercise10.tf
@@ -1,1 +1,11 @@
+variable "activities" {
+  default = ["eat", "sleep", "code", "repeat"]
+}
 
+locals {
+  list_activities = "→ ${join(" → ", var.activities)}"
+}
+
+output "activities_string" {
+  value = local.list_activities
+}
