f9c658f12b0af571d196cc1a0cd9e2c5bc39f8f2
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-19 19:02:23 -0400

    Update Exercise 11.tf

--- Exercise 11.tf
+++ Exercise 11.tf
@@ -5,3 +5,9 @@ variable "foods" {
 output "replace_burger" {
  value = replace("burger", "bur", "cheese")
 }
+variable "Marriott" {
+ default = ["pizza", "burger", "sushi", "tacos"]
+}
+output "replace_Marriott" {
+ value = replace("Marriott", "M*rr**tt")
+}
