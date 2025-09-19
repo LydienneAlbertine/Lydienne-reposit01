c92a0777540cf6ba40ba0b6ea792f6a3ca16e545
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-19 18:14:13 -0400

    Update Exercise 9.tf

--- Exercise 9.tf
+++ Exercise 9.tf
@@ -1,3 +1,8 @@
 variable "hotels" {
  default = ["Marriott", "Hilton", "Sheraton", "Hyatt"]
 }
+locals {
+ labels_upper = toset([for s in var.labels : upper(s)])
+}
+output "labels_upper" {
+ value = local.labels_upper
