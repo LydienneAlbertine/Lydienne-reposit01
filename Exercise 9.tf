f0ee35eb4734d7a7a881943bb8fc511a6b855020
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-19 18:30:37 -0400

    Update Exercise 9.tf

--- Exercise 9.tf
+++ Exercise 9.tf
@@ -1,9 +1,9 @@
-variable "hotels" {
+variable "hotels_Hyatt" {
  default = ["Marriott", "Hilton", "Sheraton", "Hyatt"]
 }
 locals {
- hotel_Hyatt_upper = toset([for s in var.hotels_hyatt : upper(s)])
+ hotels_Hyatt_upper = toset([for s in var.hotels_hyatt : upper(s)])
 }
-output "hotel_Hyatt_upper" {
+output "hotels_Hyatt_upper" {
  value = local.hotel_hyatt_upper
 }
