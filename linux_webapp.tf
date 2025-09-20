11d3fd548dbc445057ed4de909ab3d61e462f67f
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-19 21:36:21 -0400

    Update linux_webapp.tf

--- linux_webapp.tf
+++ linux_webapp.tf
@@ -17,6 +17,29 @@ resource "azurerm_linux_web_app" "app" {
  location            = azurerm_resource_group.rg.location
  resource_group_name = azurerm_resource_group.rg.name
  service_plan_id     = azurerm_service_plan.plan.id
+ https_only = true
+
+ site_config {
+   # Pick your runtime:
+   application_stack {
+     # Example for Node
+     node_version = "20-lts"
+
+     # Or Python:
+     # python_version = "3.12"
+     #
+     # Or .NET:
+     # dotnet_version = "8.0"
+   }
+
+   always_on = true
+ }
+
+ app_settings = {
+   WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+   SCM_DO_BUILD_DURING_DEPLOYMENT      = "true"
+ }
 }
+
 
 
