e047d6f46ebecc38d0408ae91d280e8728b07764
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-22 19:29:47 -0400

    Update Linuxwebapp2.tf

--- Linuxwebapp2.tf
+++ Linuxwebapp2.tf
@@ -1,28 +1,27 @@
-resource "azurerm_resource_group" "rg2" {
- name     = "${var.project_app1}-rg2"
- location = var.location2
+resource "azurerm_linux_web_app" "apps" {
+  for_each            = toset(var.webapps)
+
+  name                = "${var.project_app2}-${each.key}"
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+  service_plan_id     = azurerm_service_plan.plan2.id
+
+  https_only = true
+
+  site_config {
+    application_stack {
+      node_version = "20-lts"
+      # ou python_version / dotnet_version selon ton besoin
+    }
+
+    always_on = true
+  }
+
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    SCM_DO_BUILD_DURING_DEPLOYMENT      = "true"
+  }
 }
-
-resource "azurerm_service_plan" "plan2" {
- name                = "${var.project_app2}-plan2"
- location            = azurerm_resource_group.rg1.location
- resource_group_name = azurerm_resource_group.rg1.name
-
- os_type = "Linux"
- sku_name = var.plan2_sku_app2
-}
-
-resource "azurerm_linux_web_app" "app2" {
- name                = "${var.project_app1}-app2"
- location            = azurerm_resource_group.rg2.location
- resource_group_name = azurerm_resource_group.rg2.name
- service_plan_id     = azurerm_service_plan.plan2.id
-
- https_only = true
-
- site_config {
-   # Pick your runtime:
-   application_stack {
      # Example for Node
      node_version = "20-lts"
 
