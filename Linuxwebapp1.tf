f37138d915bbfaaa007f85cb59abadb7ddc3aeca
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-22 19:05:20 -0400

    Update Linuxwebapp1.tf

--- Linuxwebapp1.tf
+++ Linuxwebapp1.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg1" {
 
 resource "azurerm_service_plan" "plan1" {
  name                = "${var.project_app1}-plan1"
- location            = azurerm_resource_group.rg1.location1
+ location            = azurerm_resource_group.rg1.location
  resource_group_name = azurerm_resource_group.rg1.name
 
  os_type = "Linux"
