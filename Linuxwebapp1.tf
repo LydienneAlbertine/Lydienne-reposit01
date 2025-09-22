26d750b57e548dfe8741536fcc9363cf8894dab2
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-22 18:45:08 -0400

    Update Linuxwebapp1.tf

--- Linuxwebapp1.tf
+++ Linuxwebapp1.tf
@@ -1,19 +1,19 @@
 resource "azurerm_resource_group" "rg" {
- name     = "${var.project_name}-rg"
+ name     = "${var.project_app1}-rg"
  location = var.location
 }
 
 resource "azurerm_service_plan" "plan" {
- name                = "${var.project_name}-plan"
+ name                = "${var.project_app1}-plan"
  location            = azurerm_resource_group.rg.location
  resource_group_name = azurerm_resource_group.rg.name
 
  os_type = "Linux"
- sku_name = var.plan_sku_name
+ sku_name = var.plan_sku_app1
 }
 
 resource "azurerm_linux_web_app" "app" {
- name                = "${var.project_name}-app"
+ name                = "${var.project_app1}-app"
  location            = azurerm_resource_group.rg.location
  resource_group_name = azurerm_resource_group.rg.name
  service_plan_id     = azurerm_service_plan.plan.id
