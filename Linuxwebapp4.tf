db008216cf86b231873f693b2afd0ea4e0109427
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-22 20:00:55 -0400

    Update Linuxwebapp4.tf

--- Linuxwebapp4.tf
+++ Linuxwebapp4.tf
@@ -13,7 +13,7 @@ resource "azurerm_service_plan" "plan4" {
 }
 
 resource "azurerm_linux_web_app" "app4" {
- name                = "${var.project_app4-app4"
+ name                = "${var.project_app4}-app4"
  location            = azurerm_resource_group.rg4.location
  resource_group_name = azurerm_resource_group.rg4.name
  service_plan_id     = azurerm_service_plan.plan4.id
