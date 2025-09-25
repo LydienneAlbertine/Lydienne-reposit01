bf48a2f3b4e7bdeef15fc3e7d2fddb7ea1c1be1f
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-24 20:07:03 -0400

    Update Linuxwindows.tf

--- Linuxwindows.tf
+++ Linuxwindows.tf
@@ -1,14 +1,15 @@
+# Variable avec noms des Web Apps
 variable "webapp_names" {
   type = list(string)
   default = ["wa-canada-mapleleaf","wa-canada-hockey","wa-canada-poutine","wa-canada-mountie","wa-canada-niagara","wa-canada-timhortons","wa-canada-beavertail","wa-canada-loonie","wa-canada-canoe","wa-canada-igloo"]
 }
+
 # Groupe de ressources
 resource "azurerm_resource_group" "mcitazurerm" {
   name     = "septemberazurerm"
   location = "Canada Central"
 }
-
-# Service Plan (partagé entre toutes les apps)
+# Service Plan
 resource "azurerm_service_plan" "mcitsplanok" {
   name                = "mcitserviceplan"
   resource_group_name = azurerm_resource_group.mcitazurerm.name
@@ -16,10 +17,11 @@ resource "azurerm_service_plan" "mcitsplanok" {
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
-# Création des 10 Web Apps avec count
+
+# Création des Web Apps avec for_each
 resource "azurerm_linux_web_app" "mcitwebapps" {
-  count               = length(var.webapp_names)
-  name                = var.webapp_names[count.index]
+  for_each            = { for name in var.webapp_names : name => name }
+  name                = each.key
   location            = azurerm_resource_group.mcitazurerm.location
   resource_group_name = azurerm_resource_group.mcitazurerm.name
   service_plan_id     = azurerm_service_plan.mcitsplanok.id
@@ -30,9 +32,12 @@ resource "azurerm_linux_web_app" "mcitwebapps" {
     }
   }
 }
-
-# Sortie : noms des web apps créées
+# Output : noms des web apps
 output "webapp_names" {
-  value = azurerm_linux_web_app.mcitwebapps[*].name
+  value = azurerm_linux_web_app.mcitwebapps
 }
 
+# Output : longueurs des noms
+output "webapp_name_lengths" {
+  value = { for name, app in azurerm_linux_web_app.mcitwebapps : name => length(app.name) }
+}
