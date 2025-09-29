7926a9f3027812940e5ea227e582a7527e420035
LydienneAlbertine <albertinewelissane@gmail.com>
2025-09-29 19:50:28 -0400

    Update storage_account.tf

--- storage_account.tf
+++ storage_account.tf
@@ -68,5 +68,5 @@ resource "azurerm_storage_account" "lydienne_account" {
     virtual_network_subnet_ids = [azurerm_subnet.lydienne_net.id]
   }
 tags = {
-environment = "staging"
+environment="staging"
 }
