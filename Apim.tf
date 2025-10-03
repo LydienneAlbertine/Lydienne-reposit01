b8d979a1786865ec58c8711680d7274366b6acb7
LydienneAlbertine <albertinewelissane@gmail.com>
2025-10-03 18:39:25 -0400

    Update Apim.tf

--- Apim.tf
+++ Apim.tf
@@ -44,7 +44,7 @@ resource "azurerm_resource_group" "rgapim" {
 # --------------------------
 resource "azurerm_api_management" "apim" {
  name                = local.apim_name
- location            = azurerm_resource_group.rgapim.locations
+ location            = azurerm_resource_group.rgapim.location
  resource_group_name = azurerm_resource_group.rgapim.name
  publisher_name      = var.publisher_name
  publisher_email     = var.publisher_email
