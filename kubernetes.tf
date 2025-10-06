c3e4ffee0b012bd797881ff29411db0d98e9a3f3
LydienneAlbertine <albertinewelissane@gmail.com>
2025-10-06 19:20:25 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,18 +1,20 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+# Groupe de ressources pour AKS
+resource "azurerm_resource_group" "rg_aks" {
+  name     = "rg_LYD4"
   location = "West Europe"
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-lydienne-LYD4"
+  location            = azurerm_resource_group.rg_aks.location
+  resource_group_name = azurerm_resource_group.rg_aks.name
+  dns_prefix          = "lydienne-aks-4"
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
   }
 
   identity {
@@ -20,59 +22,63 @@ resource "azurerm_kubernetes_cluster" "example" {
   }
 
   tags = {
-    Environment = "Production"
+    Environment = "Development"
   }
 }
 
+# Outputs
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
-
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
-    }
+}
 
 
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+# Groupe de ressources pour ACR
+resource "azurerm_resource_group" "rg_acr" {
+  name     = "rg_LYD5"
   location = "West Europe"
 }
 
-resource "azurerm_container_registry" "example" {
-  name                = "containerregistrylydienne"  # en minuscules, obligatoire
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+# Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "acrlydienneLYD5"
+  resource_group_name = azurerm_resource_group.rg_acr.name
+  location            = azurerm_resource_group.rg_acr.location
   sku                 = "Premium"
-}
+  admin_enabled       = false
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
   }
 
-  identity {
-    type = "SystemAssigned"
+  georeplications {
+    location                = "North Europe"
+    zone_redundancy_enabled = true
   }
 
   tags = {
-    Environment = "Production"
+    Environment = "Development"
   }
 }
 
-resource "azurerm_role_assignment" "example" {
-  principal_id                     = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
+# Role Assignment pour connecter AKS au ACR
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.example.id
+  scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
 }
+
+# Output du login server du registre
+output "acr_login_server" {
+  value     = azurerm_container_registry.acr.login_server
+  sensitive = true
+}
+
