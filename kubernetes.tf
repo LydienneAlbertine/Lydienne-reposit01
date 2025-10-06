34c460d6e187cabdfd1ea35caf827aefddcb48e2
LydienneAlbertine <albertinewelissane@gmail.com>
2025-10-06 19:00:30 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -33,4 +33,47 @@ output "kube_config" {
   value = azurerm_kubernetes_cluster.example.kube_config_raw
 
   sensitive = true
+    }
+  }
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "example" {
+  name                = "containerregistrylydienne"  # en minuscules, obligatoire
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Premium"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "azurerm_role_assignment" "example" {
+  principal_id                     = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.example.id
+  skip_service_principal_aad_check = true
 }
